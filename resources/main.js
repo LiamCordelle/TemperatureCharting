86aaa1643452d92138fc08a0057470f8c07a50de
Liam Cordelle <liam.cordelle@mahifx.com>
2019-05-23 13:42:41 +1200

    Small fixes (now to make it pretty)

--- resources/main.js
+++ resources/main.js
@@ -1,23 +1,29 @@
 function redraw() {
   var g = new Dygraph(
     document.getElementById("graph"),
-    "http://192.168.1.13:8082/data"
+    "http://127.0.0.1:8082/data"
   );
 
   processData();
 }
 
 function processData() {
-  fetch("http://192.168.1.13:8082/data").then(response => {
-    var rows = response.split("\n")
+  fetch("http://127.0.0.1:8082/data").then(response => {
+    return response.text();
+  }).then(data => {
+    var rows = data.split("\n")
 
     var min = 100;
     var max = -100;
     var total = 0;
     var count = 0;
 
-    for (var i = 0; i < rows.length; i++) {
-      var temperature = rows[i].split(",")[1];
+    // Start at 1 to skip the header row
+    for (var i = 1; i < rows.length; i++) {
+      var row = rows[i]
+      if (row.indexOf(",") == -1) continue;
+
+      var temperature = parseFloat(row.split(",")[1]);
 
       if (temperature < min) {
         min = temperature;
@@ -30,9 +36,9 @@ function processData() {
       count++;
     }
 
-    document.getElementById("maxTemp").value = max;
-    document.getElementById("minTemp").value = min;
-    document.getElementById("averageTemp").value = total/count;
+    document.getElementById("maxTemp").innerHTML = max;
+    document.getElementById("minTemp").innerHTML = min;
+    document.getElementById("averageTemp").innerHTML = total/count;
   });
 }
 
